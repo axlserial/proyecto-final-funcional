5d355a78679699ba3cf8b7c303bfdb2a63614eb4
kevyx-gif <apolortms@gmail.com>
2023-02-03 17:16:31 -0600

    F

--- main.py
+++ main.py
@@ -1,129 +1,113 @@
-from flet import Page,MainAxisAlignment,CrossAxisAlignment,Row,Card,Text,colors,border,border_radius,app,IconButton,Container,Stack,alignment,icons,WEB_BROWSER
+from flet import Page,MainAxisAlignment,CrossAxisAlignment,Row,Card,Text,colors,border,border_radius,app,IconButton,Container,Stack,alignment,icons,WEB_BROWSER,Column,ListTile,Image,ImageFit,TextAlign
 import requests as rq
 
-
 def main(page:  Page):
+    #Funcion que hace la peticion
+    def generate_request(url,params):
+        response = rq.get(url, params=params)
+        if response.status_code == 200:
+            return response.json()
+
     page.title = "Flet counter example"
     page.vertical_alignment =  MainAxisAlignment.CENTER
     page.horizontal_alignment =  CrossAxisAlignment.CENTER
 
-    #Funcion que hace la peticion
-    def generate_request(url,params):
-        response = rq.get(url, params=params)
+    requestF = []
+    for i in range(1,64):
+        info = generate_request("https://pokeapi.co/api/v2/pokemon/"+str(i),"")
+        
+        if(len(info['types'])>1):
+            requestF.append({"nombre":info['name'],"tipo":(info['types'][0]['type']['name'],info['types'][1]['type']['name']),"foto":info['sprites']['other']['official-artwork']['front_default']})
+        else:
+            requestF.append({"nombre":info['name'],"tipo":info['types'][0]['type']['name'],"foto":info['sprites']['other']['official-artwork']['front_default']})
 
-        if response.status_code == 200:
-
-            return response.json()
-
-
-    data = generate_request("https://pokeapi.co/api/v2/pokemon/1","")
-    txt_number =  Text(value="1",text_align="center",width=50)
-    r =  Row(wrap=True, scroll="none", expand=True)
-    for i in range(1,16):
-        r.controls.append(
-            Card(
-                content= Container(
-                    Text(f"Item {i}"),
-                    width=100,
-                    height=100,
-                    padding=10,
-                    alignment= alignment.top_center,
-                    bgcolor= colors.RED,
-                    border= border.all(1,  colors.RED_400),
-                    border_radius= border_radius.all(5),
-                ),
-                width=200,
-                height=200,
-            )
-        )
-    
-    #Funcion de control de paginas
+        #Funcion de control de paginas
     def next_page(e):
         r.controls.clear()
-        if int(txt_number.value) <= 2:
+        if int(txt_number.value) <= 6:
             txt_number.value = int(txt_number.value) +1
-            for i in range(1+((int(txt_number.value)-1)*15),(int(txt_number.value)*15)+1):
-                r.controls.append(
-                    Card(
-                        content= Container(
-                            Text(f"Item {i}"),
-                            width=100,
-                            height=100,
-                            padding=10,
-                            alignment= alignment.top_center,
-                            bgcolor= colors.RED,
-                            border= border.all(1,  colors.RED_400),
-                            border_radius= border_radius.all(5),
-                        ),
-                        width=200,
-                        height=200,
-                    )
-                )
-            page.update()
+            cards()
         else:
             txt_number.value = 1
-            for i in range(1,16):
-                r.controls.append(
-                    Card(
-                        content= Container(
-                            Text(f"Item {i}"),
-                            width=100,
-                            height=100,
-                            padding=10,
-                            alignment= alignment.top_center,
-                            bgcolor= colors.RED,
-                            border= border.all(1,  colors.RED_400),
-                            border_radius= border_radius.all(5),
-                        ),
-                        width=200,
-                        height=200,
-                    )
-                )
-            page.update()
+            cards()
 
     def before_page(e):
         r.controls.clear()
         if int(txt_number.value) == 1:
-            txt_number.value = 3
-            for i in range(1+((int(txt_number.value)-1)*15),(int(txt_number.value)*15)+1):
-                r.controls.append(
-                    Card(
-                        content= Container(
-                            Text(f"Item {i}"),
-                            width=100,
-                            height=100,
-                            padding=10,
-                            alignment= alignment.top_center,
-                            bgcolor= colors.RED,
-                            border= border.all(1,  colors.RED_400),
-                            border_radius= border_radius.all(5),
-                        ),
-                        width=200,
-                        height=200,
-                    )
-                )
-            page.update()
+            txt_number.value = 7
+            cards()
         
         else:
             txt_number.value = int(txt_number.value) - 1
-            for i in range(1+((int(txt_number.value)*15)-15),(int(txt_number.value)*15)+1):
+            cards()
+
+    def cards():
+        for i in range((int(txt_number.value)*9)-8,(int(txt_number.value)*9)+1):
+            if type(requestF[i-1]['tipo']) == tuple:
                 r.controls.append(
                     Card(
-                        content= Container(
-                            Text(f"Item {i}"),
-                            width=100,
-                            height=100,
-                            padding=10,
-                            alignment= alignment.top_center,
+                        Container(
+                            Column(
+                                [
+                                    Text(requestF[i-1]["nombre"], text_align = "center" , selectable=False),
+                                    Image(
+                                        src=requestF[i-1]['foto'],
+                                        width=100,
+                                        height=100,
+                                    ),
+                                    Row(
+                                        [Text(requestF[i-1]["tipo"][0],selectable=False), Text(requestF[i-1]["tipo"][1],selectable=False)],
+                                        alignment=MainAxisAlignment.CENTER,
+                                    ),
+                                    
+                                ],
+                                alignment=MainAxisAlignment.CENTER,
+                                horizontal_alignment = CrossAxisAlignment.CENTER,
+                            ),
                             bgcolor= colors.RED,
                             border= border.all(1,  colors.RED_400),
                             border_radius= border_radius.all(5),
+                            alignment = alignment.center,
                         ),
+                        
                         width=200,
                         height=200,
                     )
                 )
-            page.update()
+            else:
+                r.controls.append(
+                    Card(
+                        Container(
+                            Column(
+                                [
+                                    Text(requestF[i-1]["nombre"], text_align = "center" , selectable=False),
+                                    Image(
+                                        src=requestF[i-1]['foto'],
+                                        width=100,
+                                        height=100,
+                                    ),
+                                    Text(requestF[i-1]["tipo"],selectable=False),
+                                ],
+                                alignment=MainAxisAlignment.CENTER,
+                                horizontal_alignment = CrossAxisAlignment.CENTER,
+                            ),
+                            bgcolor= colors.RED,
+                            border= border.all(1,  colors.RED_400),
+                            border_radius= border_radius.all(5),
+                            alignment = alignment.center,
+                        ),
+                        
+                        width=200,
+                        height=200,
+                    )
+                )
+
+        page.update()
+
+
+    txt_number =  Text(value="1",text_align="center",width=50)
+    r =  Row(wrap=True, scroll="none", expand=True)
+    cards()
 
 
     
@@ -137,8 +121,8 @@ def main(page:  Page):
             ),
             border_radius=8,
             padding=5,
-            width=1000,
-            height=840,
+            width=800,
+            height=900,
             bgcolor= colors.WHITE,
             alignment= alignment.top_center,
         ),
@@ -155,4 +139,4 @@ def main(page:  Page):
 
 
 
-app(target=main, view=WEB_BROWSER)
+app(target=main)
